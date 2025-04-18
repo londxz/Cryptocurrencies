94695528d3031e14bb3d1a11be2f3321213687e4
Rodion Kholodov <rodionn39@gmail.com>
2025-04-18 12:51:32 +0300

    create project

---
+++ Cryptocurrencies/App/CryptocurrenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrenciesApp.swift
+//  Cryptocurrencies
+//
+//  Created by Родион Холодов on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
